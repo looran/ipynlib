a7423388b155dcf1897570e01e988de4cb035b24
Laurent Ghigonis <laurent@gouloum.fr>
2014-12-21 09:08:24 +0100

    table: sort by default

--- ipynlib/table.py
+++ ipynlib/table.py
@@ -12,16 +12,21 @@ def show_dict(dico):
     s += "</table>"
     return HTML(s)
 
-def show_dict_of_dict(dico):
+def show_dict_of_dict(dico, sort_columns=True, sort_lines=True):
     """ dico = { 'name1' {'k1': va, 'k2', vb}, 'name2' {'k1': vc, 'k2': vd}
     keys must be identical (k1, k2) in all sub dictionnaries """
     s = "<table style=\'background: white;\'>\n<tr>\n"
     s += "<th>NAME</th>\n"
-    keys = sorted(dico[dico.keys()[0]].keys())
+    keys = dico[dico.keys()[0]].keys()
+    if sort_columns:
+        keys = sorted(keys)
     for k in keys:
         s += "<th>%s</th>\n" % k
     s += "</tr>\n"
-    for name, line in dico.items():
+    items = dico.items()
+    if sort_lines:
+        items = sorted(items)
+    for name, line in items:
         s += "<tr>\n"
         s += "<td>%s</td>\n" % name
         for k in keys:
