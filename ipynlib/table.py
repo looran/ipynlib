fbbe64cb7760686a1f126409b588916436ec4fbd
Laurent Ghigonis <laurent@gouloum.fr>
2014-12-22 20:26:25 +0100

    table: show_dict_of_dict() can sort on a particular column

--- ipynlib/table.py
+++ ipynlib/table.py
@@ -15,9 +15,13 @@ def show_dict(dico, sort_columns=True):
     s += "</table>"
     return HTML(s)
 
-def show_dict_of_dict(dico, sort_columns=True, sort_lines=True):
-    """ dico = { 'name1' {'k1': va, 'k2', vb}, 'name2' {'k1': vc, 'k2': vd}
-    keys must be identical (k1, k2) in all sub dictionnaries """
+def show_dict_of_dict(dico, sort_columns=True, sort_lines=True, sort_lines_key=None):
+    """ Display HTML table for a dict of dict
+    dico = { 'name1' {'k1': va, 'k2', vb}, 'name2' {'k1': vc, 'k2': vd}
+        keys must be identical (k1, k2) in all sub dictionnaries
+    sort_lines_key = 'k2'
+        lines will be sorted by 'k2' column
+    """
     s = "<table style=\'background: white;\'>\n<tr>\n"
     s += "<th>NAME</th>\n"
     keys = dico[dico.keys()[0]].keys()
@@ -28,7 +32,10 @@ def show_dict_of_dict(dico, sort_columns=True, sort_lines=True):
     s += "</tr>\n"
     items = dico.items()
     if sort_lines:
-        items = sorted(items)
+        if sort_lines_key:
+            items = sorted(items, key=lambda x: x[1][sort_lines_key])
+        else:
+            items = sorted(items)
     for name, line in items:
         s += "<tr>\n"
         s += "<td>%s</td>\n" % str(name)
